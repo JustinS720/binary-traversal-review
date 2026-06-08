cfd790500838ce9bb036bae41f311886681927e4
JustinS720 <274596103+JustinS720@users.noreply.github.com>
2026-06-08 11:23:35 -0700

    Added tripleSum method

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -23,7 +23,11 @@ public class TraversalReview {
      * @return three times the sum of the nodes in the tree
      */
     public static int tripleSum(TreeNode node) {
-        return -1;
+        if (node == null) {
+            return 0;
+        }
+
+        return 3 * node.data + tripleSum(node.left) + tripleSum(node.right);
     }
 
     /**
@@ -50,6 +54,9 @@ public class TraversalReview {
      * @return the sum of the positive values in the tree
      */
     public static int positiveSum(TreeNode node) {
+        if (node == null) return 0;
+
+
         return -1;
     }
 
